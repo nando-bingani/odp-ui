576dbfb834706d12eebaca2b48af39cba5098e7a
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2022-12-08 13:30:36 +0200

    Support client credentials access to the API from UI apps
    This allows interacting with the API when the user is anonymous.

--- odp/ui/base/__init__.py
+++ odp/ui/base/__init__.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import redis
 from flask import Flask
 
+from odp.client import ODPClient
 from odp.config import config
 from odp.ui.base import forms, templates
 from odp.ui.client import ODPUIClient
@@ -12,6 +13,10 @@ STATIC_DIR = Path(__file__).parent / 'static'
 TEMPLATE_DIR = Path(__file__).parent / 'templates'
 
 api: ODPUIClient
+"""ODP client for user-authenticated API access."""
+
+cli: ODPClient
+"""ODP client for client-authenticated API access."""
 
 
 def init_app(app: Flask, *, is_odp_client: bool = True):
@@ -30,6 +35,18 @@ def init_app(app: Flask, *, is_odp_client: bool = True):
                 decode_responses=True,
             ),
             app=app,
+        )
+
+        # TODO: we might want to support using a distinct client, with its
+        #  own (limited) scope, for client-credentials access to the API
+        #  from a UI application
+        global cli
+        cli = ODPClient(
+            api_url=app.config['API_URL'],
+            hydra_url=config.HYDRA.PUBLIC.URL,
+            client_id=app.config['CLIENT_ID'],
+            client_secret=app.config['CLIENT_SECRET'],
+            scope=app.config['CLIENT_SCOPE'],
         )
 
     forms.init_app(app)
