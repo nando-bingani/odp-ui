cfb2392f1f33f42c08dfe0a93aa3259046a4bc78
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2023-11-15 13:03:07 +0200

    Add select item/all checkboxes

---
+++ odp/ui/base/static/scripts/controls.js
@@ -0,0 +1,28 @@
+function initCheckAll() {
+    $('#check-all').prop('indeterminate', false);
+
+    let checkedAll = false;
+    const checkedCount = $('input:checked[id^="check-item-"]').length;
+
+    if (checkedCount > 0) {
+        const itemCount = $('input[id^="check-item-"]').length;
+        if (checkedCount < itemCount) {
+            $('#check-all').prop('indeterminate', true);
+        } else {
+            checkedAll = true;
+        }
+    }
+
+    $('#check-all').prop('checked', checkedAll);
+    $('label[for="check-all"]').text(`Select ${checkedAll ? 'none' : 'all'}`);
+}
+
+function checkAll() {
+    const checked = $('#check-all').is(':checked');
+    $('input[id^="check-item-"]').prop('checked', checked);
+    $('label[for="check-all"]').text(`Select ${checked ? 'none' : 'all'}`);
+}
+
+function checkItem() {
+    initCheckAll();
+}
