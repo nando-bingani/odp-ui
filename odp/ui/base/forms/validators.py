eca06c94b8d6aba3deb5301c59de3bbfbf6966f1
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2024-05-17 13:25:22 +0200

    Refactor JSON object validator
    Make it a callable as per the standard WTForms validator pattern.

--- odp/ui/base/forms/validators.py
+++ odp/ui/base/forms/validators.py
@@ -5,14 +5,18 @@ from werkzeug.utils import secure_filename
 from wtforms import ValidationError
 
 
-def json_object(form, field):
+def json_object():
     """A JSONTextField validator that ensures the value is a JSON object."""
-    try:
-        obj = json.loads(field.data)
-        if not isinstance(obj, dict):
-            raise ValidationError('The value must be a JSON object.')
-    except json.JSONDecodeError:
-        raise ValidationError('Invalid JSON')
+
+    def validator(form, field):
+        try:
+            obj = json.loads(field.data)
+            if not isinstance(obj, dict):
+                raise ValidationError('The value must be a JSON object.')
+        except json.JSONDecodeError:
+            raise ValidationError('Invalid JSON')
+
+    return validator
 
 
 def file_required(message='Please select a file.'):
